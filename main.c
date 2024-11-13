d48657f0b66e9d17e0ed41cacff9a2e07d9f389a
StrydrS <strydr8@gmail.com>
2024-11-13 11:54:58 -0700

    rudimentary score evaluation

--- main.c
+++ main.c
@@ -1367,10 +1367,134 @@ void perft_test(int depth) {
 
 }
 
+int material_score[12] = {
+  100, //wp
+  300, //wn
+  350, //wb 
+  500, //wr
+  1000, //wq
+  10000, //wk 
+  -100, //bp
+  -300, //bn
+  -350, //bb 
+  -500, //br
+  -1000, //bq
+  -10000, //bk
+  };
+
+const int pawn_score[64] = 
+{
+    90,  90,  90,  90,  90,  90,  90,  90,
+    30,  30,  30,  40,  40,  30,  30,  30,
+    20,  20,  20,  30,  30,  30,  20,  20,
+    10,  10,  10,  20,  20,  10,  10,  10,
+     5,   5,  10,  20,  20,   5,   5,   5,
+     0,   0,   0,   5,   5,   0,   0,   0,
+     0,   0,   0, -10, -10,   0,   0,   0,
+     0,   0,   0,   0,   0,   0,   0,   0
+};
+
+const int knight_score[64] = 
+{
+    -5,   0,   0,   0,   0,   0,   0,  -5,
+    -5,   0,   0,  10,  10,   0,   0,  -5,
+    -5,   5,  20,  20,  20,  20,   5,  -5,
+    -5,  10,  20,  30,  30,  20,  10,  -5,
+    -5,  10,  20,  30,  30,  20,  10,  -5,
+    -5,   5,  20,  10,  10,  20,   5,  -5,
+    -5,   0,   0,   0,   0,   0,   0,  -5,
+    -5, -10,   0,   0,   0,   0, -10,  -5
+};
+
+const int bishop_score[64] = 
+{
+     0,   0,   0,   0,   0,   0,   0,   0,
+     0,   0,   0,   0,   0,   0,   0,   0,
+     0,   0,   0,  10,  10,   0,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,  10,   0,   0,   0,   0,  10,   0,
+     0,  30,   0,   0,   0,   0,  30,   0,
+     0,   0, -10,   0,   0, -10,   0,   0
+
+};
+
+const int rook_score[64] =
+{
+    50,  50,  50,  50,  50,  50,  50,  50,
+    50,  50,  50,  50,  50,  50,  50,  50,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,   0,  20,  20,   0,   0,   0
+
+};
+
+const int king_score[64] = 
+{
+     0,   0,   0,   0,   0,   0,   0,   0,
+     0,   0,   5,   5,   5,   5,   0,   0,
+     0,   5,   5,  10,  10,   5,   5,   0,
+     0,   5,  10,  20,  20,  10,   5,   0,
+     0,   5,  10,  20,  20,  10,   5,   0,
+     0,   0,   5,  10,  10,   5,   0,   0,
+     0,   5,   5,  -5,  -5,   0,   5,   0,
+     0,   0,   5,   0, -15,   0,  10,   0
+};
+
+const int mirror_score[128] =
+{
+	a1, b1, c1, d1, e1, f1, g1, h1,
+	a2, b2, c2, d2, e2, f2, g2, h2,
+	a3, b3, c3, d3, e3, f3, g3, h3,
+	a4, b4, c4, d4, e4, f4, g4, h4,
+	a5, b5, c5, d5, e5, f5, g5, h5,
+	a6, b6, c6, d6, e6, f6, g6, h6,
+	a7, b7, c7, d7, e7, f7, g7, h7,
+	a8, b8, c8, d8, e8, f8, g8, h8
+};
+
+static inline int evaluate() { 
+  //static evaluation score
+  int score = 0; 
+
+  U64 bitboard;
+
+  int piece, square; 
+  
+  for(int bb_piece = P; bb_piece <=k; bb_piece++) { 
+    bitboard = bitboards[bb_piece];
+    while(bitboard) { 
+      piece = bb_piece;
+      square = get_lsb_index(bitboard);
+      
+      //score material weights
+      score += material_score[piece];
+      switch(piece) {
+        //evaluate white piece positional scores
+        case P:  score += pawn_score[square]; break;
+        case N:  score += knight_score[square]; break;
+        case B:  score += bishop_score[square]; break;
+        case R:  score += rook_score[square]; break;
+        case K:  score += king_score[square]; break; 
+        //evaluate black piece positional scores
+        case p:  score -= pawn_score[mirror_score[square]]; break;
+        case n:  score -= knight_score[mirror_score[square]]; break;
+        case b:  score -= bishop_score[mirror_score[square]]; break;
+        case r:  score -= rook_score[mirror_score[square]]; break;
+        case k:  score -= king_score[mirror_score[square]]; break; 
+      }
+      pop_bit(bitboard, square);
+    }
+  }
+  //return final evaluation based on side
+  return (side == white) ? score : -score;
+}
 /************ Search Position ************/
 
 void search_position(int depth) { 
-
   //best move placeholder
   printf("bestmove d2d4\n");
 }
@@ -1514,7 +1638,12 @@ void init_all() {
 int main() { 
   init_all(); 
   
-  uci_loop();
+  int debug = 1;
   
+  if(debug) { 
+    parse_fen("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1 ");
+    print_board(); 
+    printf("score: %d\n", evaluate());
+  } else uci_loop();
   return 0;
 }
